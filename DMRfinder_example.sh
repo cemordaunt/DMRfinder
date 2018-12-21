e7406867855410b17c740179df88df6dbff69519
Charles Mordaunt <cemordaunt@ucdavis.edu>
2018-12-20 17:30:23 -0800

    Update DMRfinder_example.sh

--- DMRfinder_example.sh
+++ DMRfinder_example.sh
@@ -2,9 +2,9 @@
 #
 #SBATCH --workdir=/share/lasallelab/programs/DMRfinder/test
 #SBATCH --partition=production				# cluster partition
-#SBATCH --mem=48000                        # total memory
-#SBATCH --time=1-0							# time (day-hr)
-#SBATCH -n 2								# cores
+#SBATCH --mem=48000                   # total memory
+#SBATCH --time=1-0							      # time (day-hr)
+#SBATCH -n 2								          # cores
 
 module load R
 
