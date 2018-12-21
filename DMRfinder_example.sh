077776080fca0e9eddc0eb46f90d1611d45ad9bc
Charles Mordaunt <cemordaunt@ucdavis.edu>
2018-12-20 16:18:43 -0800

    Add example code

---
+++ DMRfinder_example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --workdir=/share/lasallelab/programs/DMRfinder/test
+#SBATCH --partition=production				# cluster partition
+#SBATCH --mem=100000                        # total memory
+#SBATCH --time=1-0							# time (day-hr)
+#SBATCH -n 6								# cores
+
+module load R
+
+Rscript --vanilla DMRfinder5.R -n 21 -d /share/lasallelab/programs/DMRfinder/test -c 3 -e 3 -a hg38 -o DFtest --mc.cores=6 CTRL1_ CTRL2_ CTRL3_ EXP1_ EXP2_ EXP3_
